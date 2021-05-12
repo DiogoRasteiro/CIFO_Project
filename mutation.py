ea025c4554e70fc22d217dfb7dd4596822e1776b
RPeixoto10 <71514641+RPeixoto10@users.noreply.github.com>
2021-05-12 18:27:36 +0100

    XO e MU programados e primeiro treino da populacão

--- mutation.py
+++ mutation.py
@@ -1,18 +1,7 @@
 from copy import deepcopy
-from random import uniform
+from random import uniform, sample
 import numpy as np
 from operator import add
-
-def template_mutation(individual):
-    """[summary]
-
-    Args:
-        individual ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return individual
 
 def test_mutation(individual):
     """Mutation that simply copies the individual, used for testing purposes
@@ -47,3 +36,48 @@ def geometric_mutation(individual):
     fetus = list(map(add, fetus, r_weights))
 
     return fetus
+
+def swap_mutation(individual):
+
+    """Implementation of swap mutation.
+
+    Args:
+        individual (Individual): The original individual to be mutated
+
+    Returns:
+        Individual: A new individual resulting from geometric mutation of the original
+    """
+
+    # Get two mutation points
+    mut_points = sample(range(len(individual)), 2)
+    # Rename to shorten variable name
+    i = individual
+
+    i[mut_points[0]], i[mut_points[1]] = i[mut_points[1]], i[mut_points[0]]
+
+    return i
+
+
+def inversion_mutation(individual):
+    """Implementation of swap mutation.
+
+    Args:
+        individual (Individual): The original individual to be mutated
+
+    Returns:
+        Individual: A new individual resulting from geometric mutation of the original
+    """
+
+    i = individual
+    # Position of the start and end of substring
+    mut_points = sample(range(len(i)), 2)
+    # This method assumes that the second point is after (on the right of) the first one
+    # Sort the list
+    mut_points.sort()
+    # Invert for the mutation
+    i[mut_points[0]:mut_points[1]] = i[mut_points[0]:mut_points[1]][::-1]
+
+    return i
+
+
+
